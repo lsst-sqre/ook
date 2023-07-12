e1dde0d937a2719ae6df206caea1179f626d737b
Jonathan Sick <jsick@lsst.org>
2023-07-12 16:07:18 -0400

    Migrate Avro schemas into AvroBaseModel/Pydantic
    The Kafka message schemas that Ook manages are now Pydantic models
    (AvroBaseModel via dataclasses_avroschema)

---
+++ tests/domain/test_kafka.py
@@ -0,0 +1,21 @@
+"""Tests for the Kafka models in ``ook.domain.kafka``."""
+
+from __future__ import annotations
+
+import json
+
+from ook.domain.kafka import LtdUrlIngestV1, UrlIngestKeyV1
+
+
+def test_url_ingest_key_v1() -> None:
+    """Test ``UrlIngestKeyV1``."""
+    schema = json.loads(UrlIngestKeyV1.avro_schema())
+    assert schema["name"] == "url-ingest-key-v1"
+    assert schema["namespace"] == "lsst.square-events.ook"
+
+
+def test_ltd_url_ingest_v1() -> None:
+    """Test the ``LtdUrlIngestV1`` model."""
+    schema = json.loads(LtdUrlIngestV1.avro_schema())
+    assert schema["name"] == "ltd-url-ingest-v1"
+    assert schema["namespace"] == "lsst.square-events.ook"
