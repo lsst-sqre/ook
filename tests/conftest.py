13bf121725dd1f3b316153781bc46b03c592ade5
Jonathan Sick <jsick@lsst.org>
2023-07-17 12:07:31 -0400

    Add tests for the metadata endpoints

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,32 @@ from __future__ import annotations
 from collections.abc import AsyncIterator
 
 import pytest_asyncio
+from asgi_lifespan import LifespanManager
+from fastapi import FastAPI
 from httpx import AsyncClient
+
+from ook import main
 
 
 @pytest_asyncio.fixture
 async def http_client() -> AsyncIterator[AsyncClient]:
     async with AsyncClient() as client:
+        yield client
+
+
+@pytest_asyncio.fixture
+async def app() -> AsyncIterator[FastAPI]:
+    """Return a configured test application.
+
+    Wraps the application in a lifespan manager so that startup and shutdown
+    events are sent during test execution.
+    """
+    async with LifespanManager(main.app):
+        yield main.app
+
+
+@pytest_asyncio.fixture
+async def client(app: FastAPI) -> AsyncIterator[AsyncClient]:
+    """Return an ``httpx.AsyncClient`` configured to talk to the test app."""
+    async with AsyncClient(app=app, base_url="https://example.com/") as client:
         yield client
