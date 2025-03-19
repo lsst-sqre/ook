5830e9ce8e866d15deba21d7edf37b3658c1616d
Jonathan Sick <jsick@lsst.org>
2025-03-19 10:03:18 -0400

    Fully mock github resources for sdm schemas
    These resources, along with the GitHubMocker class allows us to mock all
    requests made to GitHub by the SdmSchemasIngestService when ingesting
    its w.2025.10 release.

--- tests/data/github/sdm_schemas/README.md
+++ tests/data/github/sdm_schemas/README.md
@@ -1,5 +1,8 @@
 # Repo data sample using sdm_schemas
 
+This directory contains sample data from the `lsst/sdm_schemas` repository on GitHub.
+The data is used to test the `post_ingest_sdm_schemas` endpoint and the SdmSchemasIngestService class in general.
+
 `repo.json`:
 
 ```
@@ -16,6 +19,10 @@ gh api -H "Accept: application/vnd.github+json" \
 "/repos/lsst/sdm_schemas/git/trees/w.2025.10?recursive=1"
 ```
 
+> ![NOTE]
+> This tree is modified slightly to remove reference to unused schema browser markdown files.
+> This reduces the number of blobs that need to be included in this directory to fully test the ingest service with the `w.2025.10` release of lsst/sdm_schemas.
+
 `release.json`:
 
 ```
@@ -30,4 +37,52 @@ gh api -H "Accept: application/vnd.github+json" \
 gh api -H "Accept: application/vnd.github+json" \
 -H "X-GitHub-Api-Version: 2022-11-28" \
 "/repos/lsst/sdm_schemas/git/blobs/0c9b215caeddf9e32dad6c7d26fb067fd81e4a98"
+```
+
+`dp02_dc2_blob.json`:
+
+```
+gh api -H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"/repos/lsst/sdm_schemas/git/blobs/0ae398f33adf59e647d766eabd5a3cc0f592026e" > dp02_dc2_blob.json
+```
+
+`dp02_obscore_blob.json`:
+
+```
+gh api -H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"/repos/lsst/sdm_schemas/git/blobs/9a18717c8a1a7d0f71d989558f621ad59718487d" > dp02_obscore_blob.json
+```
+
+`dp03_10yr_blob.json`:
+
+```
+gh api -H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"/repos/lsst/sdm_schemas/git/blobs/9049111d8d24f455b96bb96f70dd521aee5c0e7c" > dp03_10yr_blob.json
+```
+
+`dp03_1yr_blob.json`:
+
+```
+gh api -H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"/repos/lsst/sdm_schemas/git/blobs/e8d4603384bfecbb41b4a6958211385d8de7412b" > dp03_1yr_blob.json
+```
+
+`dp02_md_blob.json`:
+
+```
+gh api -H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"/repos/lsst/sdm_schemas/git/blobs/6f7ae30cf2e14075a479164d2d23fc0a046a78bd" > dp02_md_blob.json
+```
+
+`dp03_md_blob.json`:
+
+```
+gh api -H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"/repos/lsst/sdm_schemas/git/blobs/d58c2ca33825ef17f13d1846f0d51b17eecfec7d" > dp03_md_blob.json
 ```
--- tests/support/github.py
+++ tests/support/github.py
@@ -2,11 +2,16 @@
 
 from __future__ import annotations
 
+import json
+from pathlib import Path
+
 import respx
 
 # This mock is adapted for lsst-sqre/mobu.
 
 __all__ = ["GitHubMocker"]
+
+GITHUB_DATA_DIR = Path(__file__).parent.parent / "data" / "github"
 
 
 class GitHubMocker:
@@ -32,3 +37,89 @@ class GitHubMocker:
         self.router.get(
             url__regex=r"/repos/(?P<owner>[^/]+)/(?P<repo>[^/]+)/installation",
         ).respond(json={"id": 12345})
+
+    def mock_sdm_schema_release_ingest(
+        self, owner: str = "lsst", repo: str = "sdm_schemas"
+    ) -> None:
+        """Mock the GitHub interactions used by
+        SdmSchemasIngestService.ingest().
+        """
+        sdm_data_dir = GITHUB_DATA_DIR / "sdm_schemas"
+        release_data = (sdm_data_dir / "release.json").read_text()
+        self.router.get(
+            url=f"/repos/{owner}/{repo}/releases/latest",
+        ).respond(
+            json=json.loads(release_data),
+        )
+
+        # Mock getting repo
+        repo_data = (sdm_data_dir / "repo.json").read_text()
+        self.router.get(
+            url=f"/repos/{owner}/{repo}",
+        ).respond(
+            json=json.loads(repo_data),
+        )
+
+        # Mock get_recursive_git_tree
+        tree_data = (sdm_data_dir / "tree.json").read_text()
+        self.router.get(
+            url=f"/repos/{owner}/{repo}/git/trees/w.2025.10?recursive=1"
+        ).respond(
+            json=json.loads(tree_data),
+        )
+
+        # Mock getting the blob for deployed-schemas.txt
+        blob_data = (sdm_data_dir / "deployed_schemas_blob.json").read_text()
+        self.router.get(
+            url="/repos/lsst/sdm_schemas/git/blobs/0c9b215caeddf9e32dad6c7d26fb067fd81e4a98"
+        ).respond(
+            json=json.loads(blob_data),
+        )
+
+        # Mock getting the blob for dp02_dc2_blob.json
+        blob_data = (sdm_data_dir / "dp02_dc2_blob.json").read_text()
+        self.router.get(
+            url="/repos/lsst/sdm_schemas/git/blobs/0ae398f33adf59e647d766eabd5a3cc0f592026e"
+        ).respond(
+            json=json.loads(blob_data),
+        )
+
+        # Mock getting the blob for dp02_obscore_blob.json
+        blob_data = (sdm_data_dir / "dp02_obscore_blob.json").read_text()
+        self.router.get(
+            url="/repos/lsst/sdm_schemas/git/blobs/9a18717c8a1a7d0f71d989558f621ad59718487d"
+        ).respond(
+            json=json.loads(blob_data),
+        )
+
+        # Mock getting the blob for dp03_10yr_blob.json
+        blob_data = (sdm_data_dir / "dp03_10yr_blob.json").read_text()
+        self.router.get(
+            url="/repos/lsst/sdm_schemas/git/blobs/9049111d8d24f455b96bb96f70dd521aee5c0e7c"
+        ).respond(
+            json=json.loads(blob_data),
+        )
+
+        # Mock getting the blob for dp03_1yr_blob.json
+        blob_data = (sdm_data_dir / "dp03_1yr_blob.json").read_text()
+        self.router.get(
+            url="/repos/lsst/sdm_schemas/git/blobs/e8d4603384bfecbb41b4a6958211385d8de7412b"
+        ).respond(
+            json=json.loads(blob_data),
+        )
+
+        # Mock getting the blob dp02_md_blob.json
+        blob_data = (sdm_data_dir / "dp02_md_blob.json").read_text()
+        self.router.get(
+            url="/repos/lsst/sdm_schemas/git/blobs/6f7ae30cf2e14075a479164d2d23fc0a046a78bd"
+        ).respond(
+            json=json.loads(blob_data),
+        )
+
+        # Mock getting the blob dp03_md_blob.json
+        blob_data = (sdm_data_dir / "dp03_md_blob.json").read_text()
+        self.router.get(
+            url="/repos/lsst/sdm_schemas/git/blobs/d58c2ca33825ef17f13d1846f0d51b17eecfec7d"
+        ).respond(
+            json=json.loads(blob_data),
+        )
