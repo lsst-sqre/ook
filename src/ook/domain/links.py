6721adcbe3f7043064e66b33cc9014903fda72bd
Jonathan Sick <jsick@lsst.org>
2025-04-08 17:18:53 -0400

    Convert links domain models to Pydantic
    By converting the domain models for links into Pydantic models we can
    now directly import results from the SQLAlchemy queries in LinkStore
    into domain objects using model_validate with the "from_attributes=True"
    (i.e., ORM mode) argument.
    
    This will pave the way towards pagination of link results.

--- src/ook/domain/links.py
+++ src/ook/domain/links.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
+from pydantic import BaseModel, Field
 
 __all__ = [
     "Link",
@@ -16,92 +16,80 @@ __all__ = [
 ]
 
 
-@dataclass(slots=True, kw_only=True)
-class Link:
-    """A link to documentation."""
+class Link(BaseModel):
+    """A link to documentation.
 
-    html_url: str
-    """The URL to the documentation page."""
+    This is a Pydantic model to facilitate the parsing of SQLAlchemy queries
+    directly into the domain.
+    """
 
-    title: str
-    """The title of the documentation."""
+    html_url: str = Field(description="The URL to the documentation page.")
 
-    type: str
-    """The type of documentation."""
+    title: str = Field(description="The title of the documentation.")
 
-    collection_title: str | None = None
-    """The title of the collection of documentation this link refers to."""
+    type: str = Field(description="The type of documentation.")
+
+    collection_title: str | None = Field(
+        None,
+        description=(
+            "The title of the collection of documentation this link refers to."
+        ),
+    )
 
 
-@dataclass(slots=True, kw_only=True)
 class SdmSchemaLink(Link):
     """A link to an SDM schema's documentation."""
 
-    name: str
-    """The name of the schema."""
+    name: str = Field(description="The name of the schema.")
 
 
-@dataclass(slots=True, kw_only=True)
 class SdmTableLink(Link):
     """A link to an SDM table's documentation."""
 
-    schema_name: str
-    """The name of the schema."""
+    schema_name: str = Field(description="The name of the schema.")
 
-    name: str
-    """The name of the table."""
+    name: str = Field(description="The name of the table.")
 
 
-@dataclass(slots=True, kw_only=True)
 class SdmColumnLink(Link):
     """A link to an SDM column's documentation."""
 
-    schema_name: str
-    """The name of the schema."""
+    schema_name: str = Field(description="The name of the schema.")
 
-    table_name: str
-    """The name of the table."""
+    table_name: str = Field(description="The name of the table.")
 
-    name: str
-    """The name of the column."""
+    name: str = Field(description="The name of the column.")
 
 
-@dataclass(slots=True, kw_only=True)
-class LinksCollection[T: Link]:
-    """A collection of links to documentation of a specific entity."""
+class LinksCollection[T: Link](BaseModel):
+    """A collection of links to documentation of a specific entity.
 
-    links: list[T]
-    """The documentation links."""
+    This is a Pydantic model to facilitate the parsing of SQLAlchemy queries
+    directly into the domain.
+    """
+
+    links: list[T] = Field(description="The documentation links.")
 
 
-@dataclass(slots=True, kw_only=True)
 class SdmSchemaLinksCollection(LinksCollection[SdmSchemaLink]):
     """A collection of links to an SDM schema."""
 
-    schema_name: str
-    """The name of the schema."""
+    schema_name: str = Field(description="The name of the schema.")
 
 
-@dataclass(slots=True, kw_only=True)
 class SdmTableLinksCollection(LinksCollection[SdmTableLink]):
     """A collection of links to an SDM table."""
 
-    schema_name: str
-    """The name of the schema."""
+    schema_name: str = Field(description="The name of the schema.")
 
-    table_name: str
-    """The name of the table."""
+    table_name: str = Field(description="The name of the table.")
 
 
-@dataclass(slots=True, kw_only=True)
 class SdmColumnLinksCollection(LinksCollection[SdmColumnLink]):
     """A collection of links to SDM columns."""
 
-    schema_name: str
-    """The name of the schema."""
+    schema_name: str = Field(description="The name of the schema.")
 
-    table_name: str
-    """The name of the table."""
+    table_name: str = Field(description="The name of the table.")
 
-    column_name: str
-    """The name of the column."""
+    column_name: str = Field(description="The name of the column.")
