c95405d8eff011b1a1dacdcebb6d4f77bd4c001b
Jonathan Sick <jsick@lsst.org>
2025-06-26 14:39:59 -0400

    Create domain models for Resources
    These are the initial set of domain models for the Resources
    (bibliographic) API. The idea is that these resources can map to
    DataCite's schema for ease of creating DOIs.
    
    The Resource class has specialized subclasses for adding additional
    metadata for certain types of resources. For example, documents. These
    will get modeled with Joined-Table Inheritance in the database.
    
    To reference resources beyond the scope of Ook, we have the
    ExternalReference domain model. This maps to relatedIdentifier or
    relatedItem in the DataCite API. This domain model captures enough
    detail to be able to make the relationship in DOI metadata.
    
    A notable difference from our internal resources is that these resources
    have simpler author information because we don't use the author db
    tables.
    
    Contributors, relations, and external references are all nullable so
    that we don't need to load them for REST API queries.
    
    model_rebuild is needed to resolve forward references in the pydantic
    models.

---
+++ src/ook/domain/resources/_document.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+from typing import Annotated, Literal
+
+from pydantic import Field
+
+from ._class import ResourceClass
+from ._resource import Resource
+
+__all__ = ["Document"]
+
+
+class Document(Resource):
+    """A document resource with additional metadata.
+
+    For example, a technote or change-controlled document.
+    """
+
+    resource_class: Annotated[
+        Literal[ResourceClass.document],
+        Field(
+            description=(
+                "Class of the resource, used for metadata specialization."
+            ),
+        ),
+    ] = ResourceClass.document
+
+    series: Annotated[str, Field(description="Series name of the document.")]
+
+    handle: Annotated[
+        str,
+        Field(description="Project document identifier", examples=["SQR-000"]),
+    ]
+
+    generator: Annotated[
+        str | None,
+        Field(
+            description="Document generator used to create the document",
+            examples=["Documenteer 2.0.0", "Lander 2.0.0"],
+        ),
+    ] = None
+
+
+# Rebuild the model to ensure all fields are correctly set up
+Document.model_rebuild()
