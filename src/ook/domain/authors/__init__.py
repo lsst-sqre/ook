c0914d734f4331a0b7e27f18e4946ca8213efe15
Jonathan Sick <jsick@lsst.org>
2025-07-31 14:15:47 -0400

    Move author domain models into subpackage
    This makes way for adding modules with domain-level logical processing.

---
+++ src/ook/domain/authors/__init__.py
@@ -0,0 +1,7 @@
+"""Author domain models and utilities."""
+
+from __future__ import annotations
+
+from ._models import Address, Affiliation, Author, AuthorSearchResult
+
+__all__ = ["Address", "Affiliation", "Author", "AuthorSearchResult"]
