745a789fcfa317838b9db2293f32ff28cdd77a59
Jonathan Sick <jsick@lsst.org>
2025-07-31 15:00:40 -0400

    Create NameParser to prepare names for search
    This parser breaks down names written in multiple formats based
    heuristics to provide a cleaner input for the database search.

--- src/ook/domain/authors/__init__.py
+++ src/ook/domain/authors/__init__.py
@@ -3,5 +3,14 @@
 from __future__ import annotations
 
 from ._models import Address, Affiliation, Author, AuthorSearchResult
+from ._nameparser import NameFormat, NameParser, ParsedName
 
-__all__ = ["Address", "Affiliation", "Author", "AuthorSearchResult"]
+__all__ = [
+    "Address",
+    "Affiliation",
+    "Author",
+    "AuthorSearchResult",
+    "NameFormat",
+    "NameParser",
+    "ParsedName",
+]
